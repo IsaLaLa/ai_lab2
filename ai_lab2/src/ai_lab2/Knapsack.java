dedab50228a935588d64a09d1d73092b303445c4
IsaLaLa <thessan.l.93@gmail.com>
2018-12-09 14:08:31 +0100

    change basic concept (wrong in beginning)

--- ai_lab2/src/ai_lab2/Knapsack.java
+++ ai_lab2/src/ai_lab2/Knapsack.java
@@ -1,23 +1,45 @@
 package ai_lab2;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Knapsack {
 	private int benefit;
-	private int capacity; //how many items the bag can fit
-	private Item[] items;
+	private int capacity; //how much weight the knapsack is currently holding
+	private int maxCapacity; //how much weight the knapsack can hold
+	private ArrayList<Item> items; //keep track of the added items to the knapsack
 	
-	public Knapsack(int capacity) {
-		this.capacity = capacity;
-		items = new Item[capacity];
+	public Knapsack(int maxCapacity) {
+		this.maxCapacity = maxCapacity;
+		//maxCapacity.isMutable = false; //possible to make unchangable after giving value?
+		items = new ArrayList<Item>();
 	}
 	
+	/**
+	 * returns the knapsacks current capacity
+	 * @return
+	 */
 	public int getCapacity() {
 		return capacity;
 	}
 	
+	/**
+	 * returns the knapcacks maximum capacity
+	 * @return
+	 */
+	public int getMaxCapacity() {
+		return maxCapacity;
+	}
+	
 	public int calcBenefit(Item item) {
 		return (item.getValue() / item.getWeight());
+	}
+	
+	public void addItem(Item item) {
+		
+	}
+	
+	public void removeItem(Item item) {
+		
 	}
 	
 	//......
