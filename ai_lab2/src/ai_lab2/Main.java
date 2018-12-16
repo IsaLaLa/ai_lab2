302554972c8c05d16bef9ab55400c55098890528
IsaLaLa <thessan.l.93@gmail.com>
2018-12-16 12:15:10 +0100

    added comments

--- ai_lab2/src/ai_lab2/Main.java
+++ ai_lab2/src/ai_lab2/Main.java
@@ -2,15 +2,22 @@ package ai_lab2;
 
 import java.util.ArrayList;
 
+/**
+ * Main program for the multiple knapsack problem
+ * Course: Artificial Intelligence, da272a ht2018 
+ *
+ */
 public class Main {
 	
 	public static void main(String[] args) {
+		//creates a list of knapsacks
 		MultipleKnapsacks knapsacks = new MultipleKnapsacks();
 		knapsacks.addKnapsack(new Knapsack(25));
 		knapsacks.addKnapsack(new Knapsack(30));
 		knapsacks.addKnapsack(new Knapsack(15));
 		knapsacks.addKnapsack(new Knapsack(40));
 		
+		//creates a list of items
 		ArrayList<Item> items = new ArrayList<Item>();
 		items.add(new Item(2, 8));
 		items.add(new Item(6, 10));
@@ -21,6 +28,6 @@ public class Main {
 		items.add(new Item(8, 6));
 		items.add(new Item(11, 9));
 		
-		//.....
+		//not complete yet
 	}
 }
--- ai_lab2/src/ai_lab2/MultipleKnapsacks.java
+++ ai_lab2/src/ai_lab2/MultipleKnapsacks.java
@@ -6,28 +6,53 @@ public class MultipleKnapsacks {
 	private ArrayList<Knapsack> knapsacks;
 	private ArrayList<Item> items;
 
+	/**
+	 * constructor
+	 */
 	public MultipleKnapsacks() {
 		knapsacks = new ArrayList<Knapsack>();
 		items = new ArrayList<Item>();
 	}
 	
+	/**
+	 * returns the list of knapsacks
+	 * @return
+	 */
     public ArrayList<Knapsack> getKnapsacks() {
         return knapsacks;
     }
     
+    /**
+     * returns the list of items
+     * @return
+     */
     public ArrayList<Item> getItems() {
         return items;
     }
     
+    /**
+     * adds a knapsack to the knapsack-list
+     * @param knapsack
+     */
     public void addKnapsack(Knapsack knapsack) {
     	if(knapsack != null) {
     		knapsacks.add(knapsack);
     	}
     }
+    
+    /**
+     * a greedy solution to the multiple knapsack problem
+     * @param items
+     */
+    public void greedySolution(ArrayList<Item> items) {
+    	//to be implemented
+    }
 	
-	public MultipleKnapsacks neighborSearch(MultipleKnapsacks knapsacks) {
+    /**
+     * solving the multiple knapsack problem by using neighborhood search
+     */
+	public void neighborSearch() {
 		//to be implemented
-		return null;
 	}
 	
 	//.....
